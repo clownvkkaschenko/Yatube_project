dd3fc27b10fc6812656e2e6e169f55a0d8821cf5
Ivan Konyshkin <kkasschenkko@gmail.com>
2022-05-08 13:17:53 +0500

    passed sprint number 3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
-LMT_PSTS = 10  # limit posts per page
+LMT_PSTS: int = 10  # limit posts per page
 
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.all()[:LMT_PSTS]
+    posts = Post.objects.select_related('group').all()[:LMT_PSTS]
     title = 'Главная страница YaTube'
     text = 'Последние обновления на сайте'
     context = {
@@ -22,7 +22,7 @@ def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     posts = group.group_list.all()[:LMT_PSTS]
-    title = f'Записи сообщества {group.title}'
+    title = f'Группа {group.title}'
     context = {
         'group': group,
         'posts': posts,
