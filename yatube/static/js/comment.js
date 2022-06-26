7621126de32b48f1aca6e71b63bfaaa708af70e0
Ivan Konyshkin <kkasschenkko@gmail.com>
2022-06-26 17:30:11 +0500

    test

--- yatube/static/js/comment.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.btn_comment').addEventListener('click', function (e) {
-    var div = document.querySelector('div.form_comments')
-    div.style.display = div.style.display === 'none' ? 'block' : 'none'
-  })
